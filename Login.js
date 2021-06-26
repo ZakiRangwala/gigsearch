c7cc41db4dae786d36c9c22003501f940bc9e160
Zaki Rangwala <zakirangwala@gmail.com>
2021-06-26 11:41:29 -0400

    initial commit

---
+++ Login.js
@@ -0,0 +1,17 @@
+import Sawo from "sawo";
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div className="Login-Container">
+        <div id="sawo-container" style="height: 300px; width: 300px;"></div>
+      </div>
+      <div>
+          
+      </div>
+    </div>
+  );
+}
+
+export default Login;
